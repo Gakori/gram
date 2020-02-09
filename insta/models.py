60958f23608e85fe4db89bc6c6c4d7393620392e
faith gakori <faithgakori506@gmail.com>
2020-02-09 16:21:30 +0300

    add field post

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    '''
+    class that defines the post content
+    '''
+    title = models.CharField(max_length=50)
+    image=models.ImageField(upload_to='images')
+    date_posted = models.DateField(default=timezone.now)
+    
+    def __str__(self):
+        return self.title
+    
+class Profile(models.Model):
+    '''
+    class that defines the user profile
+    '''
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
