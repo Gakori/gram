3197067f875773a2c45c6b71337a136cb0ed3410
faith gakori <faithgakori506@gmail.com>
2020-02-09 17:21:15 +0300

    check if profile and user forms are valid

--- insta/views.py
+++ insta/views.py
@@ -4,7 +4,7 @@ from .models import Post
 from django.views.generic import ListView
 from django.contrib import messages
 from .models import Post
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
@@ -36,6 +36,24 @@ def register(request):
 
 @login_required
 def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    
     return render(request, 'insta/profile.html')
     
     
