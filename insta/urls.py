2364485544f9c87ba6215ecd6f16598b682812df
faith gakori <faithgakori506@gmail.com>
2020-02-11 13:57:30 +0300

    remove create ciew url

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PostListView
-# PostCreateView
+# from .views import PostCreateView
+# PostListView
 
 urlpatterns=[
-    path('', PostListView.as_view(), name='insta-home'),
+    # path('', PostListView.as_view(), name='insta-home'),
     
     # path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
